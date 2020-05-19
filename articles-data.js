1e7123d58741addc714267e01cad0e47a3af7fc5
Łukasz Grolik <lukasz@grolik.pl>
2020-05-20 00:26:16 +0200

    Update

---
+++ articles-data.js
@@ -0,0 +1,42 @@
+window.ARTICLES = [
+    {
+        title: "Flamingos",
+        date: "2020-05-12",
+        content: "Flamingos are a type of wading bird which four species are distributed throughout the Americas, including the Caribbean, and two species are native to Africa, Asia, and Europe.",
+        thumbnail: "../assets/flaming-5140842_640.jpg",
+        thumbnailInfo: "A couple of flamingos in the water",
+        tags: ["flamingos", "animals"]
+    },
+    {
+        title: "Roasted coffee beans",
+        date: "2020-05-12",
+        content: "Coffee is usually sold in a roasted state, and with rare exceptions, such as infusions from green coffee beans, coffee is roasted before it is consumed.",
+        thumbnail: "../assets/coffee-5149246_640.jpg",
+        thumbnailInfo: "Roasted coffee beans",
+        tags: ["coffee", "drinks"]
+    },
+    {
+        title: "aaa",
+        date: "2020-05-12",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        thumbnail: "../assets/bench-5177709_640.jpg",
+        thumbnailInfo: "Flamingos",
+        tags: ["Lorem ipsum", "Dolor"]
+    },
+    {
+        title: "aaa",
+        date: "2020-05-12",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        thumbnail: "../assets/lotus-5143420_640.jpg",
+        thumbnailInfo: "Flamingos",
+        tags: ["Lorem ipsum", "Dolor"]
+    },
+    {
+        title: "Snails",
+        date: "2020-05-12",
+        content: "Snails have considerable human relevance, including as food items, as pests, and as vectors of disease, and their shells are used as decorative objects and are incorporated into jewelry.",
+        thumbnail: "../assets/snail-5170660_640.jpg",
+        thumbnailInfo: "A snail",
+        tags: ["snails", "animals"]
+    },
+];
