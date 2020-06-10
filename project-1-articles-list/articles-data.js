178921d02718a560e31721e90fcec242893b5e21
Łukasz Grolik <lukasz@grolik.pl>
2020-06-10 12:40:37 +0200

    Update folders

--- project-1-articles-list/articles-data.js
+++ project-1-articles-list/articles-data.js
@@ -3,7 +3,7 @@ window.ARTICLES = [
         title: "Flamingos",
         date: "2020-06-07",
         content: "Flamingos are a type of wading bird which four species are distributed throughout the Americas, including the Caribbean, and two species are native to Africa, Asia, and Europe.",
-        thumbnail: "../assets/flaming-5140842_640.jpg",
+        thumbnail: "../images/flaming-5140842_640.jpg",
         thumbnailInfo: "A couple of flamingos in the water",
         tags: ["flamingos", "animals"]
     },
@@ -11,7 +11,7 @@ window.ARTICLES = [
         title: "Roasted coffee beans",
         date: "2020-05-31",
         content: "Coffee is usually sold in a roasted state, and with rare exceptions, such as infusions from green coffee beans, coffee is roasted before it is consumed.",
-        thumbnail: "../assets/coffee-5149246_640.jpg",
+        thumbnail: "../images/coffee-5149246_640.jpg",
         thumbnailInfo: "Roasted coffee beans",
         tags: ["coffee", "drinks"]
     },
@@ -19,7 +19,7 @@ window.ARTICLES = [
         title: "Foliage",
         date: "2020-05-20",
         content: "Leaves are collectively referred to as foliage. In most leaves, the primary photosynthetic tissue is located on the upper side of the blade of the leaf but in some species palisade mesophyll is present on both sides.",
-        thumbnail: "../assets/bench-5177709_640.jpg",
+        thumbnail: "../images/bench-5177709_640.jpg",
         thumbnailInfo: "A foliage scene with wooden bridge",
         tags: ["leaves", "wood", "nature"]
     },
@@ -27,7 +27,7 @@ window.ARTICLES = [
         title: "Flowers",
         date: "2020-05-14",
         content: "Many flowers have evolved to be attractive to animals, so as to cause them to be vectors for the transfer of pollen. After fertilization, the ovary of the flower develops into fruit containing seeds.",
-        thumbnail: "../assets/lotus-5143420_640.jpg",
+        thumbnail: "../images/lotus-5143420_640.jpg",
         thumbnailInfo: "A flower",
         tags: ["flowers", "nature"]
     },
@@ -35,7 +35,7 @@ window.ARTICLES = [
         title: "Snails",
         date: "2020-05-12",
         content: "Snails have considerable human relevance, including as food items, as pests, and as vectors of disease, and their shells are used as decorative objects and are incorporated into jewelry.",
-        thumbnail: "../assets/snail-5170660_640.jpg",
+        thumbnail: "../images/snail-5170660_640.jpg",
         thumbnailInfo: "A snail",
         tags: ["snails", "animals"]
     },
